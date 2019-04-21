156ddeac29777a3d26c4f73ced53742260210ce6
Patrick Hulin <patrick.hulin@gmail.com>
2019-04-21 01:48:06 -0400

    Build out CRUD for organization users.

--- backend/routes/organizations.js
+++ backend/routes/organizations.js
@@ -9,6 +9,7 @@ var passport = require('../passport');
 var db = require('../sql');
 var models = require('../models');
 var Organization = models.Organization;
+var OrganizationUser = models.OrganizationUser;
 var User = models.User;
 
 var jsonParser = bodyParser.json();
@@ -67,6 +68,81 @@ router.post('/', jsonParser, async function(req, res, next) {
     }
 
     res.json(org.toJSON());
+  } catch (e) {
+    console.error(e);
+    return next(e);
+  }
+});
+
+async function requireOrganizationAdmin(req, res, next) {
+  await db.ready();
+
+  try {
+    let orgIdStr = req.params.organization;
+    if (!orgIdStr.match(/^[0-9]+$/)) {
+      console.error('Bad Organization ID.');
+      return next(createError(400));
+    }
+    let orgId = parseInt(orgIdStr);
+    let orgs = await req.user.getOrganizations({
+      where: { id: orgId },
+    });
+    console.log('Orgs:', orgs);
+    if (!orgs) { return next(createError(404)); }
+    if (!orgs[0].OrganizationUser.admin) { return next(createError(401)); }
+
+    req.organization = orgs[0];
+    return next();
+  } catch (e) {
+    console.error(e);
+    return next(e);
+  }
+}
+
+router.get('/:organization/users', requireOrganizationAdmin, async function(req, res, next) {
+  try {
+    res.json({ results: await req.organization.getUsers() });
+  } catch (e) {
+    console.error(e);
+    return next(e);
+  }
+});
+
+router.get('/:organization/admins', requireOrganizationAdmin, async function(req, res, next) {
+  try {
+    let users = await req.organization.getUsers();
+    let admins = users.filter(u => u.OrganizationUser.admin);
+    res.json({ results: admins });
+  } catch (e) {
+    console.error(e);
+    return next(e);
+  }
+});
+
+router.post('/:organization/users', jsonParser, requireOrganizationAdmin, async function(req, res, next) {
+  try {
+    if (!req.body || !req.body.users || !req.body.users.every(isString)) {
+      return next(createError(400));
+    }
+
+    await Promise.all(req.body.users.map(email => User.findOrCreate({ where: { email } })));
+    await req.organization.addUsers(req.body.users);
+    res.json({ success: true });
+  } catch (e) {
+    console.error(e);
+    return next(e);
+  }
+});
+
+router.post('/:organization/admins', jsonParser, requireOrganizationAdmin, async function(req, res, next) {
+  try {
+    if (!req.body || !req.body.users || !req.body.users.every(isString)) {
+      return next(createError(400));
+    }
+
+    await Promise.all(req.body.users.map(email => User.findOrCreate({ where: { email } })));
+    await req.organization.addUsers(req.body.users, { through: { admin: true } });
+    res.json({ success: true });
   } catch (e) {
     console.error(e);
     return next(e);
