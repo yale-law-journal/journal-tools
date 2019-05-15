0025f31c440c741c78220b1f57248ab0f2f344ef
Patrick Hulin <patrick.hulin@gmail.com>
2019-05-15 09:35:51 -0400

    Move build dir.

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -2,6 +2,6 @@
 
 bucket=$1
 
-aws s3 sync --exclude "stats.json" --acl public-read --content-type "application/javascript; charset=utf-8" dist/ "s3://$bucket/javascripts/"
+aws s3 sync --exclude "stats.json" --acl public-read --content-type "application/javascript; charset=utf-8" dist/javascripts "s3://$bucket/javascripts/"
 aws s3 sync --exclude "*" --include "*.html" --acl public-read --content-type "text/html; charset=utf-8" public/ "s3://$bucket/"
 aws s3 sync --exclude "*.js" --exclude "*.html" --acl public-read public/ "s3://$bucket/"
