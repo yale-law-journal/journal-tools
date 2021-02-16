e4f6463444f0da5c8449d605d95a3923abb36b25
Patrick Hulin <patrick.hulin@gmail.com>
2021-02-15 19:59:06 -0500

    more cleanup.

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
-var fs = require('fs');
-var path = require('path');
-var YAML = require('yaml');
+const fs = require('fs');
+const path = require('path');
+const YAML = require('yaml');
 
+let configYaml = {};
 try {
-  var configYaml = YAML.parse(fs.readFileSync(path.resolve(__dirname, 'secrets.yml'), { encoding: 'utf-8' }));
+  configYaml = YAML.parse(fs.readFileSync(path.resolve(__dirname, 'secrets.yml'), { encoding: 'utf-8' }));
   console.log('Config:', configYaml);
 } catch (e) {
   console.error(e);
 }
-var stage = process.env.STAGE ? process.env.STAGE : 'local';
+const stage = process.env.STAGE ? process.env.STAGE : 'local';
 
 module.exports = Object.assign(configYaml.default, configYaml[stage]);
